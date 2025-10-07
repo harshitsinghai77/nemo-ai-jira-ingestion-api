2ab29721098342d81247b7435182284d113f837c
harshitsinghai <harshitsinghai77@gmail.com>
2025-10-07 11:24:29 +0530

    Add function url

--- infrastructure/lambda_stack.py
+++ infrastructure/lambda_stack.py
@@ -1,4 +1,4 @@
-from aws_cdk import Stack, aws_apigateway, aws_lambda, Duration
+from aws_cdk import Stack, aws_apigateway, aws_lambda, Duration, CfnOutput
 
 from constructs import Construct
 
@@ -7,16 +7,22 @@ class NemoAIJiraIngestionAPILambdaStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        fn = aws_lambda.Function(
+        base_lambda = aws_lambda.Function(
             self,
             "NemoAIJiraIngestionAPILambdaFunction",
             runtime=aws_lambda.Runtime.PYTHON_3_13,
-            handler="app.lambda_handler",
+            handler="main.lambda_handler",
             #code=aws_lambda.Code.from_asset("src")
             code = aws_lambda.Code.from_asset("lambda_function.zip"),
             memory_size=256,
             timeout=Duration.seconds(180)
         )
+
+        function_url = base_lambda.add_function_url(
+            auth_type=aws_lambda.FunctionUrlAuthType.NONE
+        )
+
+        CfnOutput(self, "LambdaFunctionUrl", value=function_url.url)
 
         # endpoint = aws_apigateway.LambdaRestApi(
         #     self,
